7d377b531a8a00b262634faa68ca18876cd4d1cc
damianwilinski <damian.wilinski@gmail.com>
2026-08-03 20:08:49 +0200

    chore: bump the demo to 1.17 (13) for the tester round

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "net.bunny.android.demo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.16"
+        versionCode = 13
+        versionName = "1.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
