41a0c427947fbe88378b8c95be4c9bf99c0930b1
damianwilinski <damian.wilinski@gmail.com>
2026-09-08 15:16:31 +0200

    chore: bump the demo to 1.25 (21) for the DNS sinkhole fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "net.bunny.android.demo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.24"
+        versionCode = 21
+        versionName = "1.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
